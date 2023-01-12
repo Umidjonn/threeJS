4c6abcc6bd271bc7ddc05edb445e2a21ad760c76
Umidjonn <bolbekovumid1001@gmail.com>
2023-01-13 03:15:31 +0500

    edit

--- src/script.js
+++ src/script.js
@@ -10,17 +10,15 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.x = 0.7
-mesh.position.y = -0.6
-mesh.position.z = 1
-scene.add(mesh)
+
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add( axesHelper );
 
 /**
  * Sizes
- */
+*/
 const sizes = {
     width: 800,
     height: 600
@@ -28,18 +26,19 @@ const sizes = {
 
 /**
  * Camera
- */
+*/
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
+
 /**
  * Renderer
- */
+*/
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
-console.log(mesh.position.length());
+// console.log(mesh.position.distanceTo(camera.position))
